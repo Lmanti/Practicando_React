678fdaa6e8bbb68dfaddd013243a1df86942f7fb
Lmanti <luisangelherreramantilla@gmail.com>
2021-03-30 21:54:55 -0500

    learning React from zero

--- myfirstreact/src/App(original).js
+++ myfirstreact/src/App(original).js
@@ -9,6 +9,7 @@ function App() {
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
+        <p>Proyecto de prueba hecho por Luis Herrera</p>
         <a
           className="App-link"
           href="https://reactjs.org"
